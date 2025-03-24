34ed7001c8a8360139b829c43fa111d084e7e8c9
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-03-24 16:42:22 -0300

    Método append

--- queue/main.go
+++ queue/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func main(){
+	fila := newQueue(5)
+	fmt.Println(fila)
+	fila.Append(3)
+	fmt.Println(fila.First.Data)
+	fmt.Println(fila.First.Next.Data)
+}
+
 type Node struct {
 	Data int
 	Next *Node
@@ -10,14 +18,22 @@ type Queue struct {
 	First *Node
 	Size  int
 }
-
-
 func newQueue(firstElem int) Queue{
 	return Queue{
-		&First: {Data: firstElem, Next: nil}
-		Size: 1
+		First: &Node{Data: firstElem, Next: nil},
+		Size: 1,
 	}
 }
-func (q *Queue) Append(node int){
-	
+func (q *Queue) Append(elem int){
+	newNode := &Node{Data: elem, Next: nil}
+	if q.First == nil {
+		q.First = newNode
+		return
+	}
+	current := q.First
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = newNode
+	q.Size++
 }
