bee7211485a634d60f5c90d64c8d92345a7122b9
JotaLimaTH <123770747+JotaLimaTH@users.noreply.github.com>
2025-03-27 23:36:01 -0300

    Método de inserção. Tome ponteiro.

--- binary_tree/main.go
+++ binary_tree/main.go
@@ -2,13 +2,38 @@ package main
 
 import "fmt"
 
+func main() {
+	binaryTree := NewBinaryTree(5)
+	binaryTree.Insert(5, &binaryTree.Root)
+	binaryTree.Insert(2, &binaryTree.Root)
+	binaryTree.Insert(7, &binaryTree.Root)
+	binaryTree.Insert(4, &binaryTree.Root)
+	fmt.Println(binaryTree.Root.Data, binaryTree.Root.Left.Data, binaryTree.Root.Right.Data)
+}
+
 type Node struct {
 	Data int
 	Left *Node
 	Right *Node
-	Parent *Node
 }
 
 type BinaryTree struct {
 	Root *Node
+}
+func NewBinaryTree(elem int) BinaryTree {
+	return BinaryTree{
+		Root: &Node{Data: elem},
+	}
+}
+func (b *BinaryTree) Insert(elem int, root **Node) {
+	if *root == nil {
+		*root = &Node{Data: elem}
+		return
+	}
+	
+	if elem < (*root).Data {
+		b.Insert(elem, &(*root).Left)
+	} else {
+		b.Insert(elem, &(*root).Right)
+	}
 }
