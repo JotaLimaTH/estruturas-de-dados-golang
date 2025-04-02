9e9d1746a242514f38ed91cd5b2640d0cf31c89c
JotaLimaTH <quartojota@gmail.com>
2025-04-02 10:54:07 -0300

    Traversias Pre, In e Post Order

--- binary_tree/main.go
+++ binary_tree/main.go
@@ -9,6 +9,9 @@ func main() {
 	binaryTree.Insert(7, &binaryTree.Root)
 	binaryTree.Insert(4, &binaryTree.Root)
 	fmt.Println(binaryTree.Root.Data, binaryTree.Root.Left.Data, binaryTree.Root.Right.Data)
+	fmt.Println("In Order Traversal: ", binaryTree.InOrderTraversal(binaryTree.Root))
+	fmt.Println("Pre Order Traversal: ", binaryTree.PreOrderTraversal(binaryTree.Root))
+	fmt.Println("Post Order Traversal: ", binaryTree.PostOrderTraversal(binaryTree.Root))
 }
 
 type Node struct {
@@ -36,4 +39,46 @@ func (b *BinaryTree) Insert(elem int, root **Node) {
 	} else {
 		b.Insert(elem, &(*root).Right)
 	}
+}
+func (b *BinaryTree) InOrderTraversal(root *Node) []int { //Isso vai retornar um array com os dados dos elementos da árvore
+	if root == nil {
+		return []int{}
+	}
+	var result []int
+	left := b.InOrderTraversal(root.Left)
+	right := b.InOrderTraversal(root.Right)
+
+	result = append(result, left...)
+	result = append(result, root.Data)
+	result = append(result, right...)
+
+	return result
+}
+func (b *BinaryTree) PreOrderTraversal(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+	var result []int
+	left := b.PreOrderTraversal(root.Left)
+	right := b.PreOrderTraversal(root.Right)
+
+	result = append(result, root.Data)
+	result = append(result, left...)
+	result = append(result, right...)
+
+	return result
+}
+func (b *BinaryTree) PostOrderTraversal(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+	var result []int
+	left := b.PostOrderTraversal(root.Left)
+	right := b.PostOrderTraversal(root.Right)
+
+	result = append(result, left...)
+	result = append(result, right...)
+	result = append(result, root.Data)
+
+	return result
 }
